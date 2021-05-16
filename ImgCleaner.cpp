f97a7e6f8c681554434722f8cd19e202c5183149
VV <vvkoishi@qq.com>
2021-05-16 14:18:43 +0800

    change path

--- ImgCleaner.cpp
+++ ImgCleaner.cpp
@@ -1,10 +1,12 @@
 #include "ImgCleaner.h"
 
 // Change to your path
-const path dirPath = "../";
-const path imgPath = "../assets/";
+const path dirPath = "./";
+const path imgPath = "./assets/";
 
 int main() {
+	
+	cout << "Current Path: " << current_path() << "\n";
 
 	ImgCleaner ic(dirPath, imgPath);
 	ic.Run();
