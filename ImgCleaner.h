cd1a2a2536abf665ba7ed150e43573f547af77fd
VV <vvkoishi@qq.com>
2021-05-16 14:00:50 +0800

    Update ImgCleaner.h

--- ImgCleaner.h
+++ ImgCleaner.h
@@ -98,7 +98,7 @@ private:
 			{
 				++lineno;
 				smatch matches; // matched strings go here 
-				if (regex_search(line, matches, Pattern)) 
+				while (regex_search(line, matches, Pattern)) 
 				{
 					if (matches.size() > 1 && matches[1].matched)
 					{
@@ -110,6 +110,7 @@ private:
 						imgNameTable[matches[2]]++;
 						//cout << matches[2] << "\n";
 					}
+					line = matches.suffix().str();
 				}
 			}
 		}
